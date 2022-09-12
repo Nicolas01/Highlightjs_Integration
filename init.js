16ca886dc6825efb74059091ac3f488167b41c66
Michael Gruschwitz <info@grusch-it.de>
2022-09-12 13:04:10 +0200

    Prevent loading highlight library from remote cdn

--- init.js
+++ init.js
@@ -1,26 +1,18 @@
-jQuery.cachedScript = function (url, options) {
-    // Allow user to set any option except for dataType, cache, and url
-    options = $.extend(options || {}, {
-        dataType: "script",
-        cache: true,
-        url: url
-    });
-
-    // Use $.ajax() since it is more flexible than $.getScript
-    // Return the jqXHR object so we can chain callbacks
-    return jQuery.ajax(options);
-};
-
 $(document).ready(function () {
     // load the highlight.min.js script this way to avoid Uncaught SyntaxError: unterminated regular expression literal
-    $.cachedScript('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/highlight.min.js')
-        .done(function (script, textStatus) {
+    var scriptSrc = mw.config.get('wgExtensionAssetsPath') + '/Highlightjs_Integration/highlight/highlight.min.js';
+    mw.loader
+      .getScript(scriptSrc)
+      .then(
+          function () {
+              hljs.configure({
+                  cssSelector: 'pre.code2highlight, code.code2highlight, pre.mwcode',
+              });
 
-            hljs.configure({
-                cssSelector: 'pre.code2highlight, code.code2highlight, pre.mwcode'
-            });
-
-            hljs.highlightAll();
-        });
-
-});
+              hljs.highlightAll();
+          },
+          function (e) {
+              mw.log.error(e.message, ':', scriptSrc);
+          }
+      );
+})
