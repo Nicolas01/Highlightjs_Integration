37a80929ed3277e849bc788e8e67def28fb834e6
Nicolas <2211631+Nicolas01@users.noreply.github.com>
2023-04-21 19:55:55 +0200

    fix mediawiki code not highlighted

--- init.js
+++ init.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     hljs.configure({
-        cssSelector: 'pre.code2highlight, code.code2highlight, pre.mwcode'
+        cssSelector: 'pre.code2highlight, code.code2highlight, pre.mw-code'
     });
 
     hljs.highlightAll();
